9d981febb77f680ca81ab49c7a880273259e02ce
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-27 13:14:04 +0800

    feat: add home.vue (homepage)

---
+++ pages/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-screen w-full flex items-center justify-center">
+    <div class="title">This is Homepage</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
