ec7f2ea287bb3c30aa2c0f36f5707f9ed748c614
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-27 18:55:14 +0800

    style: change icons fill to none

--- components/layouts/Sidebar.vue
+++ components/layouts/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen fixed">
-    <div class="w-28 text-center py-10 border-r-2 border-gray-600 bg-yellow-300">
+    <div class="w-28 text-center py-10 border-r-4 border-yellow-600 bg-yellow-300">
       <div class="title">
         <span class="font-black text-2xl text-gray-800">Phar</span><span class="font-black text-2xl text-yellow-600">ax</span>
       </div>
@@ -22,27 +22,27 @@ export default {
       links: [
         {
           title: 'Dashboard',
-          icon: '<svg class="w-12 h-12 fill-current text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>',
+          icon: '<svg class="w-12 h-12 text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>',
           goto: '/home'
         },
         {
           title: 'Medicine',
-          icon: '<svg class="w-12 h-12 fill-current text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>',
+          icon: '<svg class="w-12 h-12 text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>',
           goto: '/home'
         },
         {
           title: 'Masterfiles',
-          icon: '<svg class="w-12 h-12 fill-current text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path></svg>',
+          icon: '<svg class="w-12 h-12 text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>',
           goto: '/home'
         },
         {
           title: 'Invoice',
-          icon: '<svg class="w-12 h-12 fill-current text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path><path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"></path></svg>',
+          icon: '<svg class="w-12 h-12 text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path></svg>',
           goto: '/home'
         },
         {
           title: 'Search',
-          icon: '<svg class="w-12 h-12 fill-current text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>',
+          icon: '<svg class="w-12 h-12 text-yellow-600 transform hover:rotate-45 hover:text-yellow-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>',
           goto: '/home'
         }
       ]
