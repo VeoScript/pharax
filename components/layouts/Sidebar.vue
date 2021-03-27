f5613f95d658b972264a41a95ad13f57cc8bc15a
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-27 14:24:11 +0800

    feat: add sidebar component

---
+++ components/layouts/Sidebar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="h-screen w-32 bg-yellow-400">
+      <div class="title">
+        <span class="font-extrabold text-2xl text-black">Phar</span><span class="font-bold text-2xl text-yellow-400">ax</span>
+      </div>
+    </div>
+  </div>
+</template>
