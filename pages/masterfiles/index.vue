407a87024e5ca9980557eb05f785561d62dd2b2b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-27 14:07:15 +0800

    feat: add masterfiles.vue (masterfiles)

---
+++ pages/masterfiles/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen w-full flex items-center justify-center">
+    <div class="title">This is Masterfiles</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
