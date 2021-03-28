10f07600fe26dd31b58e92c54189bc91a6e3669f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-28 15:11:29 +0800

    style: sidebar change layout

--- components/layouts/Sidebar.vue
+++ components/layouts/Sidebar.vue
@@ -10,8 +10,8 @@
             <span :class="$route.path === link.goto ? 'text-yellow-800' : 'text-yellow-600'" v-html="link.icon"></span>
           </nuxt-link>
           <div class="relative">
-            <div class="absolute bottom-0 right-0 -mb-5 -mr-3">
-              <span class="bg-gray-900 transition duration-700 ease-in-out rounded-full text-white text-xs px-2 py-1" :class="$route.path === link.goto ? 'block' : 'hidden'">{{link.title}}</span>
+            <div class="absolute bottom-0 right-0 -mb-5 -mr-4">
+              <span class="bg-gray-900 w-20 transition duration-700 ease-in-out rounded-full text-white text-xs px-2 py-1" :class="$route.path === link.goto ? 'block' : 'hidden'">{{link.title}}</span>
             </div>
           </div>
         </div>
@@ -26,7 +26,7 @@ export default {
     return {
       links: [
         {
-          title: 'Dashboard',
+          title: 'POS',
           icon: '<svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>',
           goto: '/home'
         },
