1c2720bd424d1676d5cc043456c5d1613b659490
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-28 13:50:31 +0800

    feat: import POS Machine component

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,16 +2,19 @@
   <div class="h-screen">
     <Sidebar />
     <Headbar />
+    <Pos-Machine />
   </div>
 </template>
 
 <script>
 import Sidebar from '~/components/layouts/Sidebar'
 import Headbar from '~/components/layouts/Headbar'
+import PosMachine from '~/components/layouts/PosMachine'
 export default {
   components: {
     Sidebar,
-    Headbar
+    Headbar,
+    PosMachine
   },
 }
 </script>
