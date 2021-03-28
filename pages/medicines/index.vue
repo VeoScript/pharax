e316fda88a98359215a59a1d9c1a87a8c625bb75
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-28 14:41:23 +0800

    feat: create medicines page

---
+++ pages/medicines/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen">
+    <Sidebar />
+    <div class="flex ml-32 mr-10 pt-5">This is Medicines</div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/layouts/Sidebar'
+export default {
+  components: {
+    Sidebar
+  },
+}
+</script>
